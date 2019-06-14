510060eb6349f920b3353b6d212801cdf0cec348
Ryan Jacobs <ryan@rmj.us>
2019-06-14 15:04:18 -0700

    bin

--- bin_to_bc.sh
+++ bin_to_bc.sh
@@ -3,8 +3,6 @@
 binfile="$1"
 header2="E+Fri_14_Jun_2019_09:00:38_PM_UTC+shastaplus"
 
-gcc -Wall -o compdecomp compdecomp.c
-
 compdecomp "$binfile" "$binfile".h h  "$header2"
 compdecomp "$binfile".h "$binfile".c  c
 compdecomp "$binfile".c "$binfile".cbin  b
