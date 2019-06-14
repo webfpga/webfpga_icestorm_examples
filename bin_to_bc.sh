ff4e0bc7830f87ddf56a3b05489e454c3bb2900d
Ryan Jacobs <ryan@rmj.us>
2019-06-14 16:04:40 -0700

    Update bin_to_bc.sh

--- bin_to_bc.sh
+++ bin_to_bc.sh
@@ -3,6 +3,7 @@
 binfile="$1"
 header2="E+Fri_14_Jun_2019_09:00:38_PM_UTC+shastaplus"
 
+hash compdecomp || exit 0
 compdecomp "$binfile" "$binfile".h h  "$header2"
 compdecomp "$binfile".h "$binfile".c  c
 compdecomp "$binfile".c "$binfile".cbin  b
