fffc7cb8287e77ac5436e868fd428747994e6eca
Ryan Jacobs <ryan@rmj.us>
2019-08-12 01:12:30 -0700

    add compress-bitstream

--- bin_to_bc.sh
+++ bin_to_bc.sh
@@ -3,8 +3,9 @@
 binfile="$1"
 header2="E+Fri_14_Jun_2019_09:00:38_PM_UTC+shastaplus"
 
-hash compdecomp || exit 0
-compdecomp "$binfile" "$binfile".h h  "$header2"
-compdecomp "$binfile".h "$binfile".c  c
-compdecomp "$binfile".c "$binfile".cbin  b
-compdecomp "$binfile".cbin "$binfile".db db
+# Sorry, everyone... I know this binary is a bit vague.
+# We'll open-source the C code soon.
+./compress-bitstream "$binfile" "$binfile".h h  "$header2"
+./compress-bitstream "$binfile".h "$binfile".c  c
+./compress-bitstream "$binfile".c "$binfile".cbin  b
+./compress-bitstream "$binfile".cbin "$binfile".db db
